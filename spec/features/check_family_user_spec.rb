d446127fdb83c54a6cda7ebf3e291f41e03f5b60
eduardo.ekoch <eduardo.ekoch@gmail.com>
2018-07-25 08:58:35 -0300

    acrescentando itens ao teste

--- spec/features/check_family_user_spec.rb
+++ spec/features/check_family_user_spec.rb
@@ -15,10 +15,39 @@ require 'rails_helper'
       end
 
       visit new_expense_path
-      
 
+      expect(controller.current_user).to eq(user)
       expect(page).to have_content('Cadastro de Família')
       expect(user.family).to be_nil
       expect(current_path).to eq(new_family_path)
+    end
+
+    scenario 'and check_family_user successfully' do
+      user = create(:user)
+      family = create (:family)
+
+      visit root_path
+      within('.navbar') do
+        click_on 'Entrar'
+      end
+      fill_in 'Email', with: user.email
+      fill_in 'Senha', with: user.password
+      within('.form-actions') do
+        click_on 'Entrar'
+      end
+
+      visit family_path
+
+      click_on 'Cadastrar Familia'
+
+      fill_in 'Nome da Familia', with: 'Silva'
+      fill_in 'Quantidade de Pessoas', with: '5'
+      click_on 'Salvar'
+
+      expect(page).to have_content('Silva')
+
+      visit new_expense_path
+
+      expect(page).to have_content('Gastos')
     end
   end
