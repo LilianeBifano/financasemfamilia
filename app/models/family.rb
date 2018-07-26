9b3231dfab082fa88102d1901aeebce37326abdb
eduardo.ekoch <eduardo.ekoch@gmail.com>
2018-07-26 12:38:52 -0300

    Validacao do form de familia

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,10 @@
 class Family < ApplicationRecord
   has_many :users, dependent: :destroy
+
+  validates :name, :guest_total, :cep, :target, presence: true
+  validates :name, :guest_total, :cep, :target, :target_value, absence: true
+
+  validates :target_value, presence: true, numericality: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }
+
+  validates :guest_total, numericality: { only_integer: true }
 end
