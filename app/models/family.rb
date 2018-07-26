75f59a1a614c9bcf10f0f701c55ba90aff838eca
eduardo.ekoch <eduardo.ekoch@gmail.com>
2018-07-26 13:58:24 -0300

    Correcao da validacao

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,10 +1,13 @@
 class Family < ApplicationRecord
   has_many :users, dependent: :destroy
 
-  validates :name, :guest_total, :cep, :target, presence: true
-  validates :name, :guest_total, :cep, :target, :target_value, absence: true
+  validates :name, :guest_total, :cep, :target_value, :target, presence: true
 
-  validates :target_value, presence: true, numericality: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }
+  validates :target_value,
+            numericality: {
+              greater_than: 0,
+              allow_blank: false
+            }
 
   validates :guest_total, numericality: { only_integer: true }
 end
