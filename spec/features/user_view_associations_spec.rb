f1ecf97578280c59e587005d74f1ee6234d70609
Eduardo Marcos <mavbvieira@gmail.com>
2018-07-26 22:00:23 -0300

    Ajuste de Testes

--- spec/features/user_view_associations_spec.rb
+++ spec/features/user_view_associations_spec.rb
@@ -26,18 +26,17 @@ feature 'User see expanses and revenues' do
   end
 
   scenario 'user login and views own expenses' do
+    family = create(:family)
+    user = create(:user, family: family)
+
     visit root_path
+    click_on 'Entrar'
 
-    click_on 'Cadastre-se'
-
-    fill_in 'Email', with: 'eduardo@gmail.com'
-    fill_in 'Senha', with: '1234567'
-    fill_in 'Confirmar', with: '1234567'
-    click_on 'Cadastrar-me'
-
-    fill_in 'Nome da Familia', with: 'Silva'
-    fill_in 'Quantidade de Pessoas', with: '5'
-    click_on 'Salvar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    within('.form-actions') do
+      click_on 'Entrar'
+    end
 
     click_on 'Inserir Gastos'
     fill_in 'Data', with: '01/08/2018'
@@ -46,7 +45,7 @@ feature 'User see expanses and revenues' do
     select 'Vestuário e acessórios', from: 'Tipo'
     click_on 'Registrar'
 
-    expect(page).to have_content('eduardo@gmail.com')
-    expect(page).to have_content('Vestuário e acessórios')
+    expect(page).to have_content('pedro@financeiro.com')
+    expect(user.expenses.count).to eq(1)
   end
 end
