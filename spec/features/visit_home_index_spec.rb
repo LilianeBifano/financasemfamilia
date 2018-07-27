91cd0e968989820b68e85c84e63f76ab3a3efd6c
Eduardo Marcos <mavbvieira@gmail.com>
2018-07-27 16:48:58 -0300

    Corrige campo de data

--- spec/features/visit_home_index_spec.rb
+++ spec/features/visit_home_index_spec.rb
@@ -9,7 +9,6 @@ feature 'visit_home_index' do
     expect(page).not_to have_content('Minhas Configurações')
     expect(page).not_to have_content('Inserir Gastos')
     expect(page).not_to have_content('Inserir Ganhos')
-    expect(page).not_to have_content('Fechar o Mês')
   end
 
   scenario 'not view login' do
@@ -30,7 +29,6 @@ feature 'visit_home_index' do
     expect(page).to have_content('Minhas Configurações')
     expect(page).to have_content('Inserir Gastos')
     expect(page).to have_content('Inserir Ganhos')
-    expect(page).to have_content('Fechar o Mês')
     expect(page).not_to have_content('Entrar')
   end
 end
