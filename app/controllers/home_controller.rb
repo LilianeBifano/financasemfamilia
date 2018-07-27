0afe725d6e31f84cafe694c292e4d45db37253f4
Marcos Vieira <mavbvieira@gmail.com>
2018-07-27 18:35:37 -0300

    ajuste grafico

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
     @chart_data = {}
+    family_users = current_user.family.users
     ExpenseType::ALL.each do |type|
-      expenses = Expense.where(expense_type: type)
+      expenses = Expense.where(expense_type: type, user: family_users)
       @chart_data[type] = expenses.sum(:value).to_i
     end
     @chart_data2 = {}
     RevenueType::ALL.each do |type|
-      revenues = Revenue.where(revenue_type: type)
+      revenues = Revenue.where(revenue_type: type, user: family_users)
       @chart_data2[type] = revenues.sum(:value).to_i
     end
+  end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'app/channels/application_cable/channel.rb'
+  add_filter 'app/channels/application_cable/connection.rb'
+  add_filter 'app/helpers/application_helper.rb'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/mailers/application_mailer.rb'
+end
+
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
